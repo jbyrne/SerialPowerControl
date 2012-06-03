c7fe6fcdbe1803ec639452815d2b5387b8dd9381
jbyrne <jakebyrne@gmail.com>
2012-06-03 04:22:19 +0100

    Added events. home turns on device 1 and 4 leave turns off devices 1-4
    Also added html formatting

--- SerialPowerControl-web/power.php
+++ SerialPowerControl-web/power.php
@@ -1,13 +1,51 @@
 <?php
-error_reporting(E_ALL);
-$chan = dechex(bindec($_GET['c']));
-$chan = '0x'.$chan;
-$chan = chr($chan);
-echo $chan;
-  shell_exec('stty -F /dev/ttyUSB0 19200');
-if(isset($_GET['p'])){
-  shell_exec("echo -n ".$chan." > /dev/ttyUSB0");	
-  shell_exec("echo -n ".$_GET['p']." > /dev/ttyUSB0");
-}
-
+	error_reporting(E_ALL); 
+	$chan = dechex(bindec($_GET['c']));
+	$chan = '0x'.$chan;
+	$chan = chr($chan);
+	shell_exec('stty -F /dev/ttyUSB0 19200');
 ?>
+<html>
+<head>
+
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+        <title>Jakes Power Control ß </title>
+        <script type="text/javascript" charset="utf-8">
+        </script>
+</head>
+ <body style="background-color: #555658; color:#D5D6D7">
+
+	<div style="text-align: center;">
+		<?php
+			if(isset($_GET['p'])){
+			  shell_exec("echo -n ".$chan." > /dev/ttyUSB0");	
+			  shell_exec("echo -n ".$_GET['p']." > /dev/ttyUSB0");
+			}else if(isset($_GET['e'])){
+				if($_GET['e']== 'home'){
+						shell_exec("echo -n ".$chan." > /dev/ttyUSB0");	
+						shell_exec("echo -n 1 > /dev/ttyUSB0");
+						shell_exec("echo -n ".$chan." > /dev/ttyUSB0");	
+						shell_exec("echo -n 4 > /dev/ttyUSB0");
+						
+						echo 'Home Power Profile <br>';
+					
+				}else if($_GET['e']== 'leave'){
+						shell_exec("echo -n ".$chan." > /dev/ttyUSB0");	
+						shell_exec("echo -n A > /dev/ttyUSB0");
+						shell_exec("echo -n ".$chan." > /dev/ttyUSB0");	
+						shell_exec("echo -n B > /dev/ttyUSB0");
+						shell_exec("echo -n ".$chan." > /dev/ttyUSB0");	
+						shell_exec("echo -n C > /dev/ttyUSB0");
+						shell_exec("echo -n ".$chan." > /dev/ttyUSB0");	
+						shell_exec("echo -n D > /dev/ttyUSB0");
+						
+						echo 'Leaving Power Profile <br>';
+				}
+			}
+		?>
+
+
+		</div>
+</body>
+</html>
